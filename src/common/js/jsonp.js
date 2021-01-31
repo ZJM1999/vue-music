9da36c12802a980810efb3ed8f9d1bb2e9d83da0
junming <1085595048@qq.com>
2021-01-31 22:10:59 +0800

    依据接口，使用jsonp方式请求数据，并使用promise封装jsonp

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,26 @@
+import originJsonp from 'jsonp'
+
+export default function jsonp(url,data,options){
+  //处理带参数的url
+  url+=url.indexOf('?')<0?'?':'&'+handle(data)
+  //所有异步请求数据一般都封装成promise
+  return new Promise((resolve,reject)=>{
+    originJsonp(url,options,(err,data)=>{
+      if(!err){
+        resolve(data)
+      }else{
+        reject(err)
+      }
+    })
+  })
+
+}
+//处理url的参数
+function handle(data){
+  var url = ''
+  for (const key in data) {
+    //考虑data中属性值为空的情况
+    url+= (data[key]!==undefined)?`&${key}=${encodeURIComponent(data[key])}`:''
+  }
+  return url
+}
